798ffbb20cb1fcf2cc30f72e5dd7f61102ecee48
huangyingw@linux_programming <huangyingw@gmail.com>
2012-06-14 22:28:35 -0500

    n

--- src/Graph.java
+++ src/Graph.java
@@ -94,43 +94,72 @@ public class Graph {
 		}
 	}
 
-	// public void DFS(Object v){
-	// setLabel(v, "VISITED");
-	// // startVertexVisit(v);
-	// for (all e ==incidentEdges(v){
-	// if getLabel(e) = "UNEXPLORED") {
-	// w ==opposite(v, e)
-	// if (getLabel(w) = "UNEXPLORED") {
-	// // preDiscoveryTraversal(v, e, w)
-	// setLabel(e, "DISCOVERY")
-	// DFS( w)
-	// // postDiscoveryTraversal(v, e, w)
-	// else
-	// setLabel(e, "BACK")
-	// // backEdgeTraversal (v, e, w)
-	// // finishVertexVisit(v)
-	// }
-	//
-	// private void setLabel(Object v, String string) {
-	// // TODO Auto-generated method stub
-	//
-	// }
+	public void DFS(Object v) {
+		Object w = null;
+		setLabel(v, "VISITED");
+		// startVertexVisit(v);
+		for (Object e : incidentEdges(v)) {
+			if (getLabel(e) == "UNEXPLORED") {
+				w = opposite(v, e);
+			}
 
-	// public void DFS(){
-	// // initResult( )
-	// for (Object u : vertices()) {
-	// setLabel(u, "UNEXPLORED");
-	// }
-	//
-	// for (all e ==edges())
-	// setLabel(e, "UNEXPLORED");
-	// for (all v ==vertices())
-	// if (getLabel(v) = "UNEXPLORED")
-	// // preComponentVisit(v)
-	// DFS(v);
-	// // postComponentVisit(v)
-	// result();
-	// }
+			if (getLabel(w) == "UNEXPLORED") {
+				// preDiscoveryTraversal(v, e, w)
+				setLabel(e, "DISCOVERY");
+				DFS(w);
+				// postDiscoveryTraversal(v, e, w)
+			} else {
+				setLabel(e, "BACK");
+				// backEdgeTraversal (v, e, w)
+			}
+		}
+		finishVertexVisit(v);
+	}
+
+	private void finishVertexVisit(Object v) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private Object opposite(Object v, Object e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Object[] incidentEdges(Object v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void setLabel(Object v, String string) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void DFS() {
+		// initResult( )
+		for (Object u : vertices()) {
+			setLabel(u, "UNEXPLORED");
+		}
+		for (Object e : edges()) {
+			setLabel(e, "UNEXPLORED");
+		}
+		for (Object v : vertices())
+			if (getLabel(v) == "UNEXPLORED")
+				// preComponentVisit(v)
+				DFS(v);
+		// postComponentVisit(v)
+		result();
+	}
+
+	public String getLabel(Object v) {
+		return null;
+	}
+
+	private void result() {
+		// TODO Auto-generated method stub
+
+	}
 
 	private Object[] edges() {
 		// TODO Auto-generated method stub
